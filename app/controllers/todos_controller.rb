d5a4474e5814fadb663778317627fc1b8764c6fe
Kevin Locke <kevinbreid@yahoo.com>
2023-05-16 17:26:29 -0700

    enabling cookies and sessions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,10 @@
 class TodosController < ApplicationController
+
+    def index
+
+        user = User.find_by(id: session[:user_id])
+        todos = user.todos
+        render json: todos
+
+    end
 end
