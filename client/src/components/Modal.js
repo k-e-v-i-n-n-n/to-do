35ab9d6df7d42ee62578e7fa97bb1e92d5743215
Kevin Breid <kevinbreid@yahoo.com>
2023-07-12 17:08:10 +0200

    editing format

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -24,36 +24,32 @@ console.log("datat", data)
       <div className="overlay">
         <div className="modal">
           <div className="form-title-container">
-          <h3>{mode} your task</h3>
-          <button>x</button>
+            <h3>{mode} your task</h3>
+            <button>x</button>
           </div>
-          <form>
-          <input
-          required
-          maxLength={30}
-          placeholder="Enter task..."
-          name="title"
-          value={data.title}
-          onChange={(e) => popForm(e)}
-          />
-          <br/>
-          <label for="range">Drag to indicate progress</label>
-          <input
-          required
-          type="range"
-          min="0"
-          max='100'
-          id="range"
-          name="progress"
-          value={data.progress}
-          onChange={(e) => popForm(e)}
-
-          />
-          <input className={mode} type="submit"/>
-
-          </form>
+            <form>
+              <input
+              required
+              maxLength={30}
+              placeholder="Enter task..."
+              name="title"
+              value={data.title}
+              onChange={(e) => popForm(e)}
+              />
+              <br/>
+              <label for="range">Drag to indicate progress</label>
+              <input
+              required
+              type="range"
+              min="0"
+              max='100'
+              id="range"
+              name="progress"
+              value={data.progress}
+              onChange={(e) => popForm(e)}/>
+              <input className={mode} type="submit"/>
+            </form>
         </div>
-           
       </div>
     )
   }
